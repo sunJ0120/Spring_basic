01b6ff4f41efd27da83c0436a413f234848ba0b6
BOOK-S08D1UN6CT\sspure1214 <sspure1214@naver.com>
2023-11-22 13:13:47 +0900

    스프링 핵심 원리 이해2 - 객체 지향 원리 적용
    새로운 할인 정책 적용과 문제점
    
    RateDiscountPolicy를 적용했지만, OrderServiceImpl이 구체적인 객체에 의존한다는 문제점이 있다.
    
    그렇다고 DiscountPolicy만 둘 시, NullPointerException이 발생할 수 있다.
    
    즉, 다른 객체가 대신해서 구체 객체를 주입해줘야 한다.

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,14 +2,19 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
-
     private final MemberRepository memberRepository = new MemoryMemberRepository(); //구현체로 생성해서 사용
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    //OrderServiceImpl을 변경해야 한다. 어찌되었든 ocp 원칙 위반이라고 할 수 있다.
+//    private final DiscountPolicy; //추상화인 interface에만 의존하도록 한다.
+    //그러나 이렇게 할 경우 nullpointException이 터진다.
+
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId); //회원 정보 조회
